b343db694b7a547709850258032fef3e8f63df72
hbrt <hebrt.br@gmail.com>
2025-02-09 19:48:37 -0300

    new

--- 04 - Data e hora/3_strftime_strptime.py
+++ 04 - Data e hora/3_strftime_strptime.py
@@ -1,12 +1,24 @@
+# from datetime import datetime
+
+# data_hora_atual = datetime.now()
+# data_hora_str = "2023-10-20 10:20"
+# mascara_ptbr = "%d/%m/%Y %a"
+# mascara_en = "%Y-%m-%d %H:%M"
+
+# print(data_hora_atual.strftime(mascara_ptbr))
+
+# data_convertida = datetime.strptime(data_hora_str, mascara_en)
+# print(data_convertida)
+# print(type(data_convertida))
 from datetime import datetime
 
 data_hora_atual = datetime.now()
-data_hora_str = "2023-10-20 10:20"
+data_hora_string = "2023-10-20 10:20"
 mascara_ptbr = "%d/%m/%Y %a"
 mascara_en = "%Y-%m-%d %H:%M"
 
 print(data_hora_atual.strftime(mascara_ptbr))
 
-data_convertida = datetime.strptime(data_hora_str, mascara_en)
+data_convertida = datetime.strptime(data_hora_string, mascara_en)
+data_convertida = data_convertida.strftime(mascara_ptbr)
 print(data_convertida)
-print(type(data_convertida))
--- 04 - Data e hora/4_pytz.py
+++ 04 - Data e hora/4_pytz.py
@@ -2,8 +2,11 @@ from datetime import datetime
 
 import pytz
 
-data = datetime.now(pytz.timezone("Europe/Oslo"))
-data2 = datetime.now(pytz.timezone("America/Sao_Paulo"))
+oslo_time = datetime.now(pytz.timezone("Europe/Oslo")).astimezone(pytz.timezone("Europe/Oslo"))
+formatted_oslo_time = oslo_time.strftime("%Y-%m-%d %H:%M:%S")
 
-print(data)
-print(data2)
+sao_paulo_time = datetime.now(pytz.timezone("America/Sao_Paulo")).astimezone(pytz.timezone("America/Sao_Paulo"))
+sao_paulo_time = sao_paulo_time.strftime("%Y-%m-%d %H:%M:%S")
+
+print(formatted_oslo_time)
+print(sao_paulo_time)
